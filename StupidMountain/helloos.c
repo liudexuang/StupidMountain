5d701314d52fe912c9f2d8e3a86e54ddf5c43cf8
liudexuang <liudexuang@hotmail.com>
2020-04-13 17:41:02 +0800

    xxx

--- StupidMountain/helloos.c
+++ StupidMountain/helloos.c
@@ -3,5 +3,22 @@
 main()
 {
     printf("hello world\n");
+
+	int i; /*变量声明。变量i是32比特的整数类型*/
+
+	for (i = 0; i <= 19; i++)
+	{
+		printf("%d", i);
+		if (i == 31839)
+		{
+			i = 7;
+		}
+		else
+		{
+			i = 5;
+		}
+	}
+
+
 }
 
